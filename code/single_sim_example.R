c6670d6d1872523b19c29f376bbf77ce9b0ed003
juansvs <juans.vs@gmail.com>
2023-09-20 16:48:00 +0200

    removed functions, moved to functions script. added figures

--- code/single_sim_example.R
+++ code/single_sim_example.R
@@ -12,23 +12,30 @@ source("functions.R")
 
 
 # simulate trajectories of two individuals
-A <- simulate_tracks(tau = 5, dp = 2, social = 0)
+A <- simulate_tracks(tau = 5, dp = 2, social = 0.9)
 # visualize
+
 plot.new()
+pdf("../docs/figures/example_tracks.pdf",width = 3,height = 3)
 par(mar = c(0.5,0.5,0.5,0.5))
 plot(A$x,A$y,type = 'n',asp=1,xlab = "", ylab = "",xaxt="n", yaxt="n")
 grid()
 sapply(1:2, \(i) lines(A$x[,i],A$y[,i], col = hcl.colors(2, palette = "Dark 3")[i]))
+dev.off()
 
 # get the utilization distributions of both individuals
 uds <- getUDs(A)
 
 # Plot UDs together
+pdf("../docs/figures/example_UDs_overlay.pdf", width = 3, height = 3)
+par(mar = c(0.5,0.5,0.5,0.5))
+plot(extent(uds[[2]]), type='n', xaxt="n",yaxt='n', asp=1)
+grid()
 plot(uds[[2]], col.grid=NA, DF = "PDF", level=0,
      col.level = hcl.colors(2, palette = "Dark 3"),
      col.DF = hcl.colors(2, palette = "Dark 3"), 
-     xaxt="n", yaxt="n",labels=c("","0.95",""))
-
+     xaxt="n", yaxt="n",labels=c("","0.95",""),add=T)
+dev.off()
 # Plot UDs separately
 image(raster(uds[[2]][[1]], DF="PMF"), asp=1, col = colorRampPalette(c("white",hcl.colors(2,"Dark 3")[1]))(18), bty = "o", xlim = range(A[[1]]),ylim = range(A[[2]]))
 image(raster(uds[[2]][[2]], DF="PMF"), asp=1, col = colorRampPalette(c("white",hcl.colors(2,"Dark 3")[2]))(18), bty = "o", xlim = range(A[[1]]),ylim = range(A[[2]]))
@@ -38,8 +45,15 @@ image(raster(uds[[2]][[2]], DF="PMF"), asp=1, col = colorRampPalette(c("white",h
 prods <- getUDprod(uds)
 
 zrng <- range(unlist(lapply(prods,lapply,cellStats,range)))
-with(list(nu = 1/(24*7)), plot(prods$UD[[2]]/nu, col = hcl.colors(255, "Plasma"), bty = "n",xaxt="n", yaxt="n", asp=1, xlab="", ylab = ""))
-plot(prods$SD[[2]], col = hcl.colors(255, "Plasma"), bty = "n",xaxt="n", yaxt="n", asp=1, xlab="", ylab = "")
+pdf("../docs/figures/example_UDprod.pdf", width = 4, height = 3,family = "sans")
+par(mar = c(0.5,0.5,0.5,4))
+with(list(nu = 1/(24*7)), raster::plot(prods$UD[[2]]/nu, col = hcl.colors(255, "Plasma"), bty = "n",xaxt="n", yaxt="n", asp=1, xlab="", ylab = ""))
+dev.off()
+
+pdf("../docs/figures/example_SDprod.pdf", width = 4, height = 3,family = "sans")
+par(mar = c(0.5,0.5,0.5,4))
+raster::plot(prods$SD[[2]], col = hcl.colors(255, "Plasma"), bty = "n",xaxt="n", yaxt="n", asp=1, xlab="", ylab = "")
+dev.off()
 
 # get correlation vector at cells with overlap
 cors <- getCorrs(A, uds[[2]])
@@ -51,14 +65,21 @@ values(corrast) <- NA
 corvals <- with(list(nu = 1/24/7), colSums(cors[[1]]*exp(-nu*(0:(nrow(cors[[1]])-1)))))
 corrast[as.numeric(names(corvals))] <- corvals
 zlim = c(-max(abs(cellStats(corrast,range))),max(abs(cellStats(corrast,range))))
-plot(corrast, col = hcl.colors(255, "Blue-Red 3") ,  yaxt="n",xaxt="n", zlim = zlim)
+pdf("../docs/figures/example_corrRast.pdf",width = 4, height = 3, family = "sans")
+raster::plot(corrast, col = hcl.colors(25, "Blue-Red 3") ,  yaxt="n",xaxt="n", zlim = zlim)
 grid()
+dev.off()
 # Plot FOI rasters
 par(mfrow = c(2,1), mar = c(0,1,0,4), pty = "s")
 plot(test[[1]], col = hcl.colors(255,"Plasma"), xaxt = "n", yaxt = "n")
 lines(extent(test[[1]]))
 grid()
 plot(log(test[[1]]), col = hcl.colors(255,"Plasma"), xaxt = "n", yaxt = "n")
+
+# A better way to visualize this is to plot the locations of the cells with correlations
+# get the coordinates of the cells
+corrcoords <- xyFromCell(corrast[[1]],Which(corrast[[1]]!=0, T))
+
 
 
 # get total FOI
@@ -162,180 +183,4 @@ outdf %>% pivot_longer(cols = starts_with("foi"), names_to = "calc", values_to =
   lm(foi~overlap+nu, data = .) %>% summary()
 # linear model shows relationship between overlap and foi. nu has a clear
 # negative effect; faster decay rates result in lower FOI
-
-
-##### --------------- FUNCTION DEFINITIONS --------------#####
-# simulate_tracks <- function(tau = 10, g = 1e3, steps = 500, dp = 0.2, social = NULL, smooth = TRUE) {
-#   nind = 2
-#   p.lambdas = matrix(rpois(nind*2, dp*g)-dp*g, ncol = 2)# home range centers
-#   p.x0 = p.lambdas # initial positions
-#   p.phi = 1 # trajectory smoothing parameter
-#   nsteps = steps
-#   
-#   xs = replicate(nind, numeric(nsteps))
-#   ys = replicate(nind, numeric(nsteps))
-#   
-#   for (s in seq_len(nind)) {
-#     x <- y <- numeric(nsteps)
-#     x[1] <- mux <- p.x0[s,1]
-#     y[1] <- muy <- p.x0[s,2]
-#     xnoise = rnorm(nsteps)
-#     ynoise = rnorm(nsteps)
-#     # take successive steps
-#     for(i in seq_len(nsteps-1)) {
-#       x[i+1] = x[i]-1/tau*(x[i]-p.lambdas[s,1])+sqrt(g)*xnoise[i]
-#       y[i+1] = y[i]-1/tau*(y[i]-p.lambdas[s,2])+sqrt(g)*ynoise[i]
-#     }
-#     xs[,s] = x
-#     ys[,s] = y
-#   }
-#   
-#   if(is.numeric(social)) {
-#     W <- replicate(nsteps, diag(nrow = nind, ncol = nind))
-#     W[1,2,] <- social
-#     # make symmetric
-#     W <- apply(W, 3, \(x) x*upper.tri(x, diag = T)+t(x*upper.tri(x)), simplify = FALSE)
-#     # convolution kernel hij(tau) is the weights matrix, divided by the row sums
-#     h_soc <- lapply(W, \(x) x/rowSums(x))
-#     # create empty matrices with the same dimensions as the position matrices
-#     xsoc <- matrix(nrow = nrow(xs), ncol = ncol(xs))
-#     ysoc <- matrix(nrow = nrow(ys), ncol = ncol(ys))
-#     # fill the array using dot product of the step*ind x and y matrices, multiplied by the rows of h_soc
-#     for (t in 1:nsteps) {
-#       xs[t,] <- h_soc[[t]]%*%xs[t,]
-#       ys[t,] <- h_soc[[t]]%*%ys[t,]
-#     }
-#   }
-#   
-#   
-#   if(smooth) {
-#     # create smoother matrix for convolution
-#     H_inl <- outer(seq_len(nsteps),seq_len(nsteps), \(x,y) abs(x-y)/p.phi*besselK(abs(x-y)/p.phi, nu = 1))
-#     diag(H_inl) <- 1
-#     H_inl <- H_inl/rowSums(H_inl)
-#     xs <- apply(xs, 2, "%*%", H_inl)
-#     ys <- apply(ys, 2, "%*%", H_inl)
-#   }
-#   list(x=xs,y=ys)
-# }
-# 
-# # Function to get the utilization distributions. Output is a list, first element
-# # are the ctmm fits, second are the individual UDs
-# getUDs <- function(X, dr = NULL) {
-#   xs <- X[[1]]
-#   ys <- X[[2]]
-#   nind <- ncol(xs)
-#   nsteps <- nrow(xs)
-#   # create move objects
-#   moveobjs <- lapply(seq_len(nind), \(c) move(x = xs[,c],y = ys[,c],time = as.POSIXct(3600*seq_len(nsteps),origin = Sys.time()), animal = paste0("ind",c), proj = "+proj=tmerc"))
-#   # convert to telemetry
-#   telemetries <- lapply(moveobjs, as.telemetry)
-#   # ctmm fit
-#   GUESS <- lapply(telemetries, \(i) ctmm.guess(i, interactive = F))
-#   FITS <- lapply(seq_along(telemetries), \(i) ctmm.select(telemetries[[i]], GUESS[[i]]))
-#   # AKDE
-#   uds <- if(is.null(dr)) akde(telemetries, FITS) else akde(telemetries, FITS, grid = list(dr = c(dr,dr)))
-#   return(list(FITS,uds))
-# }
-# 
-# getUDprod <- function(X) {
-#   uds <- X[[2]]
-#   nind <- length(uds)
-#   ### UD and SD products ###
-#   rs <- lapply(uds, raster, DF = "PMF")
-#   cellarea <- prod(res(rs[[1]]))
-#   
-#   # extend all rasters to have the same extent. 
-#   rs <- lapply(rs, extend, y = extent(do.call(merge,rs)), value = 0)
-#   # Create lists to store objects
-#   UDprods <- UDsds <- list()
-#   # Possible combinations
-#   combs <- combn(length(uds), 2)
-#   # Calculate UD and SD products from UD pair values
-#   for (i in seq_len(ncol(combs))) {
-#     ind1 <- combs[1,i]
-#     ind2 <- combs[2,i]
-#     r1 <- rs[[ind1]]
-#     r2 <- rs[[ind2]]
-#     # product of probabilities, divided by area
-#     udprob <- r1*r2
-#     UDprods[[paste(ind1,ind2,sep = "-")]] <- UDprods[[paste(ind2,ind1,sep = "-")]] <- udprob/cellarea
-#     sdprob <- sqrt(r1*(1-r1))*sqrt(r2*(1-r2))
-#     UDsds[[paste(ind1,ind2,sep = "-")]] <- UDsds[[paste(ind2,ind1,sep = "-")]] <- sdprob/cellarea
-#   }
-#   return(list(UD = UDprods, SD = UDsds))
-# }
-# # function to calculate the correlations. Output is a list, where every element
-# # is a lags by cells matrix of correlation between two individuals
-# getCorrs <- function(xy, r) {
-#   gridcors2 <- list()
-#   xs <- xy[[1]]
-#   ys <- xy[[2]]
-#   nind <- ncol(xs)
-#   nsteps <- nrow(xs)
-#   combs <- combn(nind,2)
-#   for (i in 1:ncol(combs)) {
-#     r1 <- raster(r[[i]])
-#     ind1 <- combs[1,i]
-#     ind2 <- combs[2,i]
-#     # get position histories (i.e. which cell was each individual in at every time
-#     # point). 
-#     pos1 <- cellFromXY(r1, xy = cbind(xs[,ind1],ys[,ind1]))
-#     pos2 <- cellFromXY(r1, xy = cbind(xs[,ind2],ys[,ind2]))
-#     
-#     # keep only cells that both visited at some point
-#     ovlpcells <- unique(pos1)[unique(pos1) %in% unique(pos2)]
-#     if(length(ovlpcells)==0) {
-#       # Still fill in an item in the list, but write just NA. 
-#       gridcors2[[paste(ind1,ind2,sep = "-")]] <- NA
-#       gridcors2[[paste(ind2,ind1,sep = "-")]] <- NA
-#     } else {
-#       maxlag <- nsteps-1
-#       cormat_ab <- cormat_ba <- matrix(0,nrow = nsteps, ncol = length(ovlpcells))
-#       for (j in seq_along(ovlpcells)) {
-#         cell <- ovlpcells[j]
-#         a <- b <- numeric(nsteps)
-#         a[match(cell, pos1)] <- b[match(cell, pos2)]<- 1
-#         xcorr <- ccf(a,b,lag.max = maxlag, plot = F)
-#         xcorr_vals <- as.numeric(xcorr$acf)
-#         cormat_ab[,j] <- rev(xcorr_vals[1:nsteps])
-#         cormat_ba[,j] <- xcorr_vals[nsteps:length(xcorr_vals)]
-#       }
-#       dimnames(cormat_ab) <- dimnames(cormat_ba) <- list(NULL, cell = ovlpcells)
-#       gridcors2[[paste(ind1,ind2,sep = "-")]] <- cormat_ab
-#       gridcors2[[paste(ind2,ind1,sep = "-")]] <- cormat_ba
-#     }
-#   }
-#   return(gridcors2)
-# }
-# # function to calculate the per-cell FOI. Calculates correlation within. Output
-# # is list with each element a FOI raster
-# getFOI <- function(xy, uds, beta = 1, lambda = 1/24, nu = 1/(24*7)) {
-#   foirasts2 <- list()
-#   gridcors2 <- getCorrs(xy,uds[[1]])
-#   for (i in seq_along(gridcors2)) {
-#     if (is.array(gridcors2[[i]])) {
-#       corcells <- as.numeric(colnames(gridcors2[[i]]))
-#       corvals <- numeric(length(uds[[1]][[i]]))
-#       # get lags
-#       lags <- 0:(nrow(gridcors2[[i]])-1)
-#       # scale and integrate correlation at every cell
-#       corvals[corcells] <- colSums(gridcors2[[i]]*exp(-nu*lags))
-#       udp <- uds[[1]][[i]]
-#       sdp <- uds[[2]][[i]]
-#       corrast <- udp
-#       values(corrast) <- corvals
-#       cellarea <- prod(res(corrast))
-#       
-#       foi <- beta/cellarea*lambda*(1/nu*udp*cellarea+sdp*cellarea*corrast)
-#       foi <- foi*(foi>=0)
-#     } else {
-#       foi <- beta/cellarea*lambda*(1/nu*udp*cellarea)
-#     }
-#     
-#     foirasts2[[i]] <- foi
-#   }
-#   return(foirasts2)
-# }
-
 
