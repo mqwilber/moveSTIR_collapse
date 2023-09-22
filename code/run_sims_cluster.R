325bfe0eaebb0a55e45c2ccce3d8e3fb8ff57bb1
juansvs <juans.vs@gmail.com>
2023-09-22 11:11:30 +0200

    corrected calculation of total area for each individual. Doubled the number of simulations

--- code/run_sims_cluster.R
+++ code/run_sims_cluster.R
@@ -16,7 +16,7 @@ clusterCall(cl, source,"./functions.R")
 social <- c(0,0.5, 0.7, 0.9,0.93, 0.96, 1)
 nus <- 1/(24*c(1/12, 1/3, 1, 3, 7))
 gridres <- c(5, 10, 20)
-iterations <- 10
+iterations <- 20
 
 trajs <- (replicate(iterations, lapply(social, \(x) simulate_tracks(tau = 5, dp = 2, social = x))))
 out <- list()
@@ -30,12 +30,14 @@ for (i in seq_along(trajs)) {
       cors <- getCorrs(A, uds[[2]])
       fois <- getFOI(A,prods, nu = nu)
       cellarea <- prod(res(fois[[1]]))
-      totarea <- length(fois[[1]])*cellarea
+      totareai <- summary(uds[[2]][[1]])$CI[2]
+      totareaj <- summary(uds[[2]][[2]])$CI[2]
       
       c(i,
         nu, 
         cellarea,
-        totarea,
+        totareai,
+        totareaj,
         cellStats(1*1/24/cellarea*1/nu*prods[[1]][[1]], sum), 
         cellStats(fois[[1]],sum), 
         cellStats(fois[[2]],sum), 
@@ -45,7 +47,7 @@ for (i in seq_along(trajs)) {
 
 stopCluster(cl)
 outdf <- as.data.frame(do.call(rbind, out))
-names(outdf) <- c("sim", "nu", "Ax","Atot", "foi_ud", "foi_full1", "foi_full2", "overlap")
+names(outdf) <- c("sim", "nu", "Ax","Atoti","Atotj", "foi_ud", "foi_full1", "foi_full2", "overlap")
 outdf$social <- rep(social, each = length(nus)*length(gridres))
 outname <- paste0("sim_res_", format(Sys.time(), "%y%m%d-%H%M"), ".csv")
 write.csv(outdf,outname, quote = F, row.names = F)
