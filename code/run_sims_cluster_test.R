65de2c427256d975a1b09e02a5a4c4b20108f004
juansvs <juans.vs@gmail.com>
2023-11-14 13:25:49 +0100

    edited for new fun. outputs

--- code/run_sims_cluster_test.R
+++ code/run_sims_cluster_test.R
@@ -1,3 +1,6 @@
+#' Simulations of movement test code for running sims and FOI calculations in a cluster
+#' Juan S. Vargas
+#' November 2023.
 
 # Register a parallel backend
 library(parallel)
@@ -8,9 +11,9 @@ cl <- makeCluster(length(future::availableWorkers()))
 registerDoParallel(cl)
 
 # Load functions
-source("functions.R")
+source("code/functions.R")
 # Load functions in each node
-clusterCall(cl, source,"./functions.R")
+clusterCall(cl, source,"code/functions.R")
 
 
 social <- c(0,0.5)
@@ -19,7 +22,7 @@ gridres <- c(10,5)
 iterations <- 3
 
 trajs <- (replicate(iterations, lapply(social, \(x) simulate_tracks(steps = 100,tau = 5, dp = 2, social = x))))
-### New version, edited to have only 3 grid sizes for the same sim.
+
 UDout <- foreach(i = seq_along(trajs), .packages = c("move","ctmm")) %:% foreach(gr = gridres) %dopar% {
   list(SIM = i,
        TRAJS = trajs[[i]],
@@ -34,47 +37,22 @@ out <- foreach(i = seq_along(UDout), .packages = c("move", "ctmm")) %:%
     SIM <- UDout[[i]]$SIM
     prods <- getUDprod(UDS)
     # cors <- getCorrs(A, uds[[i]][[2]])
-    fois <- getFOI(A, prods, nu = nu)
-    cellarea <- prod(res(fois[[1]]))
+    fois <- getFOI(A, UDS, nu = nu)
+    cellarea <- prod(res(fois[[1]][[1]]))
     totareai <- summary(UDS[[2]][[1]])$CI[2]
     totareaj <- summary(UDS[[2]][[2]])$CI[2]
-    
+
     c(SIM,
       nu, 
       cellarea,
       totareai,
       totareaj,
-      cellStats(1*1/24/cellarea*1/nu*prods[[1]][[1]], sum), 
-      cellStats(fois[[1]],sum), 
-      cellStats(fois[[2]],sum), 
+      cellStats(fois[[1]][[3]], sum), 
+      cellStats(fois[[1]][[1]],sum), 
+      cellStats(fois[[1]][[2]],sum), 
       overlap(UDS[[2]])$CI[,,2][1,2])
+    
   }
-### Delete if the above works as expected
-# out <- list()
-# for (i in seq_along(trajs)) {
-#   A <- trajs[[i]]
-#   out[[i]] <- foreach(gr = gridres, 
-#                       .combine = 'rbind', .packages = c("move", "ctmm"), .inorder = FALSE) %:%
-#     foreach(nu = nus, .combine = 'rbind', .inorder = FALSE) %dopar% {
-#       uds <- getUDs(A, gr)
-#       prods <- getUDprod(uds)
-#       cors <- getCorrs(A, uds[[2]])
-#       fois <- getFOI(A,prods, nu = nu)
-#       cellarea <- prod(res(fois[[1]]))
-#       totareai <- summary(uds[[2]][[1]])$CI[2]
-#       totareaj <- summary(uds[[2]][[2]])$CI[2]
-#       
-#       c(i,
-#         nu, 
-#         cellarea,
-#         totareai,
-#         totareaj,
-#         cellStats(1*1/24/cellarea*1/nu*prods[[1]][[1]], sum), 
-#         cellStats(fois[[1]],sum), 
-#         cellStats(fois[[2]],sum), 
-#         overlap(uds[[2]])$CI[,,2][1,2])
-#     }
-# } 
 
 stopCluster(cl)
 outdf <- as.data.frame(do.call(rbind,out))
